deb3647d0ca341c71d38de07f74129833a68cf15
Jesen Tanadi <jesen@Jesens-MBP.home>
2018-03-16 01:10:10 -0400

    slight modification to follow specs

--- generateArray/generateArray.js
+++ generateArray/generateArray.js
@@ -1,15 +1,15 @@
 const randomRange = require('../randomRange/randomRange')
 
-const generateArray = (minLength = 10, maxLength) => {
+const generateArray = (minLength, maxLength) => {
   /* (int[, int]) -> arr
   Returns an array of random numbers, with lengths:
-  - If only minLength is specified, array will be specified length
-  - If no length is specified, array will be random length, max 10
-  - If minLength and maxLength are specified, array will be
-    random length between those values
+  - If only 1 number is passed in, array will be specified length
+  - If no numbers are passed in, array will be random length, max 10
+  - If 2 numbers are passed in, array will be random length
+    between those values (min/max-order agnostic)
   */
 
-  let arrLength = minLength;
+  let arrLength = minLength || randomRange(10);
 
   if(typeof maxLength !== "undefined") {
     arrLength = randomRange(minLength, maxLength);
@@ -22,3 +22,5 @@ const generateArray = (minLength = 10, maxLength) => {
 
   return retArray;
 };
+
+module.exports = generateArray
