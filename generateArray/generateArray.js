0127a5eea3aa0d16a524a03eea9c91abea45168d
Jesen Tanadi <jesen@Jesens-MBP.home>
2018-03-16 00:48:50 -0400

    take out randomRange & import module

--- generateArray/generateArray.js
+++ generateArray/generateArray.js
@@ -1,3 +1,5 @@
+const randomRange = require('../randomRange/randomRange')
+
 const generateArray = (minLength = 10, maxLength) => {
   /* (int[, int]) -> arr
   Returns an array of random numbers, with lengths:
@@ -19,16 +21,4 @@ const generateArray = (minLength = 10, maxLength) => {
   }
 
   return retArray;
-};
-
-const randomRange = (min, max) => {
-  /* (int[, int]) -> int
-  Returns a random int between min and max.
-  If max is not specified, random int will be between 0 and integer passed.
-  */
-  if(typeof max === "undefined") {
-    max = min;
-    min = 0;
-  }
-  return Math.round(Math.random() * (max - min)) + min;
 };
