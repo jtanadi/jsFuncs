261460d3e29e06274f1a22e32e2aad19683e96f5
JesenTanadi <jesentanadi@JesenTanadi.local>
2018-03-19 14:31:32 -0400

    add [-1] tests, edit test signatures, removing regex bool

--- arrFuncs/tests/getIndices-test.js
+++ arrFuncs/tests/getIndices-test.js
@@ -6,12 +6,28 @@ test("returns an array", (t) => {
   t.end()
 })
 
+test("returns [-1] when item not found", (t) => {
+  const inputStrings = ["a", "b", "c"]
+  const inputNums = [1, 2, 3]
+  const inputMixed = [1, 2, 3, "a", "b", "c"]
+  
+  t.deepEqual(getIndices(inputStrings, "d"), [-1], "string input, string item")
+  t.deepEqual(getIndices(inputStrings, /1/), [-1], "string input, regex item")
+  t.deepEqual(getIndices(inputNums, 5), [-1], "nums input, num item")
+  t.deepEqual(getIndices(inputNums, /5/), [-1], "nums input, regex item")
+  t.deepEqual(getIndices(inputMixed, 5), [-1], "mixed input, num item")
+  t.deepEqual(getIndices(inputMixed, "d"), [-1], "mixed input, string item")
+  t.deepEqual(getIndices(inputMixed, /5/), [-1], "mixed input, regex item")
+  t.deepEqual(getIndices(inputMixed, /d/), [-1], "mixed input, regex item")
+  t.end()
+})
+
 test("returns correct index (strings)", (t) => {
   const input = ["a", "b", "c"]
   const regex = /a/
   t.plan(2)
   t.deepEqual(getIndices(input, "a"), [0], "pass in string as itemToIndex")
-  t.deepEqual(getIndices(input, regex, true), [0], "pass in regex as itemToIndex")
+  t.deepEqual(getIndices(input, regex), [0], "pass in regex as itemToIndex")
 })
 
 test("returns correct indices (strings)", (t) => {
@@ -20,8 +36,8 @@ test("returns correct indices (strings)", (t) => {
   const regex2 = "a"
 
   t.deepEqual(getIndices(input, "a"), [0, 3, 6], "pass in string as itemToIndex")
-  t.deepEqual(getIndices(input, regex, true), [0, 3, 6], "pass in regex as itemToIndex")
-  t.deepEqual(getIndices(input, regex2, true), [0, 3, 6], "pass in string as regex as itemToIndex")
+  t.deepEqual(getIndices(input, regex), [0, 3, 6], "pass in regex as itemToIndex")
+  t.deepEqual(getIndices(input, regex2), [0, 3, 6], "pass in string as regex as itemToIndex")
   t.end()
 })
 
@@ -31,8 +47,8 @@ test("returns correct indices (numbers)", (t) => {
   const regex2 = 1
 
   t.deepEqual(getIndices(input, 1), [0, 3, 6], "pass in num as itemToIndex")
-  t.deepEqual(getIndices(input, regex, true), [0, 3, 6], "pass in regex as itemToIndex")
-  t.deepEqual(getIndices(input, regex2, true), [0, 3, 6], "pass in num as regex as itemToIndex")
+  t.deepEqual(getIndices(input, regex), [0, 3, 6], "pass in regex as itemToIndex")
+  t.deepEqual(getIndices(input, regex2), [0, 3, 6], "pass in num as regex as itemToIndex")
   t.end()
 })
 
