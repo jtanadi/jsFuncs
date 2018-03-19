ac4a7504b3ed05a68257607d9a900e96cdb2abee
Jesen Tanadi <jesen@Jesens-MBP.home>
2018-03-18 23:21:22 -0400

    add a couple more tests

--- arrFuncs/tests/removeDupes-test.js
+++ arrFuncs/tests/removeDupes-test.js
@@ -3,22 +3,58 @@ const test = require("tape")
 
 test("base case", (t) => {
   const list = [1, 2, 3, 1, 1, 4, 5]
+  const actual = removeDupes(list)
+  const expected = [ [1, 2, 3, 4, 5], [1, 1] ]
 
-  t.deepEqual(removeDupes(list), [ [1, 2, 3, 4, 5], [1, 1] ])
+  t.deepEqual(actual, expected)
   t.end()
 })
 
-test("matchCase defaults to true", (t) => {
-  const list = ["a", "A", "b"]
-
-  t.deepEqual(removeDupes(list), [ ["a", "A", "b"], [] ])
+test("mixed-case strings: matchCase defaults to true", (t) => {
+  const list = ["a", "A", "b", "C", "c"]
+  const actual = removeDupes(list)
+  const expected = [ ["a", "A", "b", "C", "c"], [] ]
+  
+  t.deepEqual(actual, expected)
   t.end()
 })
 
-test("set matchCase to false", (t) => {
-  const list = ["a", "A", "b"]
- 
-  t.deepEqual(removeDupes(list, false), [ ["a", "b"], ["A"] ])
+test("mixed-case strings: set matchCase to false", (t) => {
+  const list = ["a", "A", "b", "C", "c"]
+  const actual = removeDupes(list, false)
+  const expected =  [ ["a", "b", "C"], ["A", "c"] ]
+
+  t.deepEqual(actual, expected)
   t.end()
 })
 
+test("pass in small list", (t) => {
+  const list = [1]
+
+  t.deepEqual(removeDupes(list), [[1], []])
+  t.end()
+})
+
+test("pass in multiple copies with different cases, matchCase is true", (t) => {
+  const list = ["aB", "AB", "Ab", "bC", "BC", "Bc", "BC", "AB"]
+  const actual = removeDupes(list)
+  const expected = [
+    ["aB", "AB", "Ab", "bC", "BC", "Bc"],
+    ["BC", "AB"]
+  ]
+
+  t.deepEqual(actual, expected)
+  t.end()
+})
+
+test("pass in multiple copies with different cases, matchCase is false", (t) => {
+  const list = ["aB", "AB", "Ab", "bC", "BC", "Bc", "BC", "AB"]
+  const actual = removeDupes(list, false)
+  const expected = [
+    ["aB", "bC"],
+    ["AB", "Ab", "BC", "Bc", "BC", "AB"]
+  ]
+
+  t.deepEqual(actual, expected)
+  t.end()
+})
