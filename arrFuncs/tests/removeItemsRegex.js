4950dce3538508429f5c38c537600685bda9a654
JesenTanadi <jesentanadi@JesenTanadi.local>
2018-03-30 10:39:57 -0400

    rename function

--- arrFuncs/tests/removeItemsRegex.js
+++ arrFuncs/tests/removeItemsRegex.js
@@ -1,11 +1,11 @@
 const test = require("tape");
-const filterOutRegex = require("../filterOutRegex");
+const removeItemsRegex = require("../removeItemsRegex");
 
 test("it works", t => {
   const inputArr = ["Hello", "532245", "how", "are", "23982938", "you", "747", "121233"];
   const regex = "[0-9]+"; // find numbers 1 or more
   
-  const actual = filterOutRegex(inputArr, regex);
+  const actual = removeItemsRegex(inputArr, regex);
   const expected = ["Hello", "how", "are", "you"];
 
   t.deepEqual(actual, expected);
