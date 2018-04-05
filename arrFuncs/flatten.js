bd06f38cf8f2712a405390acef3b403c33a5e497
Jesen Tanadi <jesen@Jesens-MBP.home>
2018-04-05 08:41:55 -0400

    refactor to use ternary

--- arrFuncs/flatten.js
+++ arrFuncs/flatten.js
@@ -1,12 +1,18 @@
 const flatten = arr => arr.reduce((retArr, item) => {
+  return retArr.concat(Array.isArray(item) ? flatten(item) : [item]);
+}, []);
+
+module.exports = flatten;
+
+/* More verbose:
+const flatten = arr => arr.reduce((retArr, item) => {
   if(Array.isArray(item)) {
     return retArr.concat(flatten(item));
   }
   retArr.push(item);
   return retArr;
 }, []);
-
-module.exports = flatten;
+*/
 
 /* non-recursive... works only for 1 arr in arr
 const flattenArray = arr => {
